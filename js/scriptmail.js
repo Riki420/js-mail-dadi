78611ba58568ca32ea86f1d184f14860bc6b1289
Riki420 <girottiriccardo1998@gmail.com>
2021-07-02 20:17:40 +0200

    boilerplate + link css e js + inizio mail

---
+++ js/scriptmail.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+var userMail = prompt('Inserisci la tua mail')
+
+console.log('email utente: ', userMail)
