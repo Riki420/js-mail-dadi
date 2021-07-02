f95f7f223c97e5ece0b87a62899088c7c54bfa2a
Riki420 <girottiriccardo1998@gmail.com>
2021-07-02 22:56:01 +0200

    Stampa email sulla pagina

--- js/scriptmail.js
+++ js/scriptmail.js
@@ -2,6 +2,14 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-var userMail = prompt('Inserisci la tua mail')
+//
+var messaggio;
+var userMail = prompt('Inserisci la tua mail');
 
-console.log('email utente: ', userMail)
+console.log('email utente: ', userMail);
+
+
+//Email Print
+
+messaggio = "La tua mail è  " + userMail;
+document.getElementById("visualizza").innerHTML = messaggio;
