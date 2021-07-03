755dd772c6f39161571c063dcbb957c296bd6a57
Riki420 <girottiriccardo1998@gmail.com>
2021-07-03 18:48:49 +0200

    aggiunti risultati numeri da 1 a 6

--- js/scriptdadi.js
+++ js/scriptdadi.js
@@ -1,17 +1,33 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
 
+
+// Variabili dei numeri
+var uNum = 
+Math.round(Math.random() * 5) + 1;
+
+var cNum = 
+Math.round(Math.random() * 5) + 1;
+
+
+// Stampa in pagina
 var userPoint =
 document.getElementById("visual").innerHTML =
-'Il tuo punteggio è: ' + Math.floor(Math.random() * 6);
+'Il tuo punteggio è: ' + uNum;
 
 var cpuPoint =
 document.getElementById("visual-2").innerHTML =
-'Il punteggio del computer è: ' + Math.floor(Math.random() * 6);
+'Il punteggio del computer è: ' + cNum;
 
 
-if(userPoint > cpuPoint){
-    document.getElementById("risultato").innerHTML = "Hai vinto la partita"
+if(cNum < uNum){
+    document.getElementById("risultato").innerHTML = 'Hai vinto la partita';
+    
+} else if(cNum == uNum){
+    document.getElementById("risultato").innerHTML = 'La partita termina in pareggio';
+
+
 } else{
-    document.getElementById("risultato").innerHTML = "Il Computer ha vinto"
+    document.getElementById("risultato").innerHTML = 'Hai perso la partita';
+
 }
