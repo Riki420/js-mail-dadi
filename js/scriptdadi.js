e2788ea4f01701739e45bed6e775c601dfc773bc
Riki420 <girottiriccardo1998@gmail.com>
2021-07-03 12:04:08 +0200

    script dadi: 2 var+ math.random+stampa su pagina

--- js/scriptdadi.js
+++ js/scriptdadi.js
@@ -1,2 +1,17 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+
+var userPoint =
+document.getElementById("visual").innerHTML =
+'Il tuo punteggio è: ' + Math.floor(Math.random() * 6);
+
+var cpuPoint =
+document.getElementById("visual-2").innerHTML =
+'Il punteggio del computer è: ' + Math.floor(Math.random() * 6);
+
+
+if(userPoint > cpuPoint){
+    document.getElementById("risultato").innerHTML = "Hai vinto la partita"
+} else{
+    document.getElementById("risultato").innerHTML = "Il Computer ha vinto"
+}
--- js/scriptmail.js
+++ js/scriptmail.js
@@ -2,14 +2,14 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-//
+// Ask Email
 var messaggio;
 var userMail = prompt('Inserisci la tua mail');
 
 console.log('email utente: ', userMail);
 
 
-//Email Print
+// Email Print
 
 messaggio = "La tua mail è  " + userMail;
 document.getElementById("visualizza").innerHTML = messaggio;
